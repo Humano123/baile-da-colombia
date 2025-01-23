c1886b6e928655f33cb189cb562e88ce6b753b94
Thiago <161526886+Humano123@users.noreply.github.com>
2025-01-23 15:56:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function adm() {
+    window.location.href = "adm.html";
+}
+function dev() {
+    window.location.href = "dev.html";
+}
+function normal() {
+    window.location.href = "player.html";
+}
